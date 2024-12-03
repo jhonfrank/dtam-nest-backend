e9f716bf75e705277bd84878e0060fee56d34712
Jhon Frank Alvarado Echigoyen <jhonfrank.ae@outlook.com>
2024-12-03 14:15:29 -0500

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const PORT = process.env.APP_PORT ?? 3000;
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api/v1');
+    await app.listen(PORT);
+}
+
+bootstrap();
