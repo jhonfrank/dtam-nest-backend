84d9996444bb2c443691fefb069256fc22156935
Jhon Frank Alvarado Echigoyen <jhonfrank.ae@outlook.com>
2024-12-05 15:26:55 -0500

    build: Configure TypeOrm and class validator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-    imports: [ConfigModule.forRoot()],
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
+            type: process.env.DB_TYPE as any,
+            host: process.env.DB_HOST,
+            port: parseInt(process.env.DB_PORT),
+            username: process.env.DB_USERNAME,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME,
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: !!process.env.DB_SYNC,
+        }),
+    ],
     controllers: [],
     providers: [],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 const PORT = process.env.APP_PORT ?? 3000;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.useGlobalPipes(new ValidationPipe());
     app.setGlobalPrefix('api/v1');
     await app.listen(PORT);
 }
